228f90a6adf5c0854ae1a0170b37b3218f354286
brian nehemia <briannehemia@gmail.com>
2021-01-20 08:12:29 +0700

    commit RabbitMQ MassTransit

---
+++ RabbitMQTest/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RabbitMQTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public TestController(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        // GET: api/<TestController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TestMessage value)
+        {
+            //Send to rabbitmq server
+            _publishEndpoint.Publish<TestMessage>(value);
+
+            return Ok();
+        }
+    }
+}
---
+++ RabbitMQTest/Services/TetsService.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using RabbitMQTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQTest.Services
+{
+    public interface ITestService
+    {
+        
+    }
+   
+    public class TetsService : ITestService, IConsumer<TestMessage>
+    {
+        public async Task Consume(ConsumeContext<TestMessage> test)
+        {
+            // get message ( Name )
+            var GetMessage = test.Message.Name;
+        }
+    }
+}
